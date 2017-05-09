044eba04e625940919a3876a4bfedc8250d74fb9
JRY <jerrieyuan@hotmail.com>
2017-05-09 17:20:44 +0800

    Updates

--- setup.js
+++ setup.js
@@ -1,48 +1,62 @@
-function appendNode(child) {
-  var parent = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0];
-  console.log(parent);
-  parent.appendChild(child);
-}
+(function() {
 
-function addJs(js) {
-  var script = document.createElement("script");
-  script.src = js;
-  script.type = "text/javascript";
-  appendNode(script);
-}
-
-function addCss(css) {
-  var link = document.createElement("link");
-  link.rel = "stylesheet";
-  link.type = "text/css";
-  link.href = css;
-  appendNode(link);
-}
-
-
-function run(config) {
-  if (typeof config.js === "undefined" || typeof config.css === "undefined") {
-    return;
+  function appendNode(child) {
+    var parent = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0];
+    console.log(parent);
+    parent.appendChild(child);
   }
-  addCss(config.css);
-  addJs(config.js);
-}
 
-(function setup() {
-  var url = 'https://raw.githubusercontent.com/JerrieYuan/uvss_api_test_tool/gh-pages/config';
-  var request = new XMLHttpRequest();
+  function addJs(js) {
+    var script = document.createElement("script");
+    script.src = js;
+    script.type = "text/javascript";
+    appendNode(script);
+  }
 
-  request.onloadend = function() {
-    var responseData = this.responseText.split('||', -1);
-    if (responseData.length < 2) {
+  function addCss(css) {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = css;
+    appendNode(link);
+  }
+
+  const isWeb = () => {
+    var REG = /^http*/gi;
+    var url = location.href;
+    if (REG.test(url)) {
+      return true;
+    }
+    return false;
+  }
+
+  function run(config) {
+    if (typeof config.js === "undefined" || typeof config.css === "undefined") {
       return;
     }
-    var config = {
-      js: responseData[0],
-      css: responseData[1]
-    };
-    run(config);
-  }.bind(request);
-  request.open("GET", url, true);
-  request.send();
+    addCss(config.css);
+    addJs(config.js);
+  }
+
+  (function setup() {
+    var url = './config';
+    if (isWeb()) {
+      url = 'https://raw.githubusercontent.com/JerrieYuan/uvss_api_test_tool/gh-pages/config';
+    }
+    var request = new XMLHttpRequest();
+
+    request.onloadend = function() {
+      var responseData = this.responseText.split('||', -1);
+      if (responseData.length < 2) {
+        return;
+      }
+      var config = {
+        js: responseData[0],
+        css: responseData[1]
+      };
+      run(config);
+    }.bind(request);
+    request.open("GET", url, true);
+    request.send();
+  })();
 })();
